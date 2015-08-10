5a21e1b7099854eac172d8eddb7b812e396b638d
leonardoCatharin <leo_catharin@hotmail.com>
2015-08-10 20:30:01 -0300

    first commit

---
+++ js/include.js
@@ -0,0 +1,74 @@
+var navItens = 
+	[
+		{
+			name: 'Cadastros',
+			href: '',
+			filhos: [
+				{
+					name: 'Paciente',
+					href: '',
+					filhos: []
+				},
+				{
+					name: 'Médico' ,
+					href: '',
+					filhos: []
+				},
+				{
+					name: 'Secretária',
+					href: '',
+					filhos: []
+				}
+			]
+		},
+		{
+			name: 'Agenda',
+			href: '',
+			filhos: [
+				{
+					name: 'Agendar Consultas',
+					href: '',
+					filhos: []
+				}
+				
+			]
+		},
+		{
+			name: 'Relatórios',
+			href: '',
+			filhos: [
+				{
+					name: 'Consultar Agenda',
+					href: '',
+					filhos: []
+				},
+				{
+					name: 'Relatório Paciente' ,
+					href: '',
+					filhos: []
+				},
+				{
+					name: 'Pacientes atendidos',
+					href: '',
+					filhos: []
+				}
+			]
+		},
+		{
+			name: 'Prontuário',
+			href: '',
+			filhos: [
+				{
+					name: 'Prontuários',
+					href: '',
+					filhos: []
+				}
+			]
+		},
+		{
+			name: 'Fechar',
+			href: '',
+			filhos:[]
+		}
+	]
+
